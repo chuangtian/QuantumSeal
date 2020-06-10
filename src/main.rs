eabb655e1eab2489dcefcd9927e4531c953dfb19
TianChuang <38176372+chuangtian@users.noreply.github.com>
2020-06-10 10:03:00 +0800

    feat: ecc

--- src/main.rs
+++ src/main.rs
@@ -22,3 +22,20 @@ const USAGE: &str = r#"quantumseal — post-quantum migration workbench (static 
 USAGE:
     quantumseal <COMMAND> [OPTIONS]
 
+COMMANDS:
+    scan <PATH>          Inventory PATH recursively and emit a CryptoBOM.
+    diff <BASELINE>      Scan and compare against a saved baseline CryptoBOM (JSON).
+    rules                Print the built-in crypto indicator catalog.
+    help                 Show this help.
+
+SCAN OPTIONS:
+    --format <json|text>     Output format (default: text).
+    --output <FILE>          Write output to FILE instead of stdout.
+    --max-depth <N>          Limit recursion depth.
+    --follow-symlinks        Follow symbolic links (off by default).
+
+DIFF OPTIONS:
+    --path <PATH>            Directory to scan for the current state (default: .).
+    --format <json|text>     Output format (default: text).
+    --fail-on-regression     Exit non-zero if new/increased crypto exposure is found.
+

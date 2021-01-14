db44036cdb1c93705f5b03dc2f96e2b6a86e37e1
TianChuang <38176372+chuangtian@users.noreply.github.com>
2021-01-14 15:24:00 +0800

    refactor: rust

--- tests/integration.rs
+++ tests/integration.rs
@@ -16,3 +16,12 @@ fn scan_fixture(rel: &str) -> CryptoBom {
 fn legacy_fixture_flags_high_risk_components() {
     let bom = scan_fixture("fixtures/legacy_service");
     let ids: Vec<&str> = bom.components.iter().map(|c| c.rule_id).collect();
+
+    // Expect Shor-breakable and deprecated primitives.
+    assert!(ids.contains(&"rsa"), "expected RSA, got {ids:?}");
+    assert!(ids.contains(&"ecc"), "expected ECC, got {ids:?}");
+    assert!(ids.contains(&"md5"), "expected MD5, got {ids:?}");
+    assert!(ids.contains(&"3des"), "expected 3DES, got {ids:?}");
+    assert!(ids.contains(&"rc4"), "expected RC4, got {ids:?}");
+
+    // The top band should be Critical (md5/sha1/3des/rc4 present).

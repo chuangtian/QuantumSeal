b87bcaf17f551110eade7fd74f533c7c2f862bfd
DuxuteLi70 <228983122+DuxuteLi70@users.noreply.github.com>
2021-01-14 14:59:00 +0800

    perf: improve cli

--- tests/integration.rs
+++ tests/integration.rs
@@ -7,3 +7,12 @@ use quantumseal::diff;
 use quantumseal::scanner::{self, ScanOptions};
 
 fn scan_fixture(rel: &str) -> CryptoBom {
+    let root = Path::new(env!("CARGO_MANIFEST_DIR")).join(rel);
+    let scan = scanner::scan(&root, &ScanOptions::default()).expect("scan should succeed");
+    CryptoBom::from_scan(rel, &scan)
+}
+
+#[test]
+fn legacy_fixture_flags_high_risk_components() {
+    let bom = scan_fixture("fixtures/legacy_service");
+    let ids: Vec<&str> = bom.components.iter().map(|c| c.rule_id).collect();

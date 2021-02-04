35be6c81ec544fac77176820ca72e6aec752a646
TianChuang <38176372+chuangtian@users.noreply.github.com>
2021-02-04 17:00:00 +0800

    chore: bump rust

--- viewer/dist/cli.d.ts
+++ viewer/dist/cli.d.ts
@@ -3,3 +3,5 @@
  * quantumseal-view — CLI for rendering a CryptoBOM.
  *
  * Usage:
+ *   quantumseal-view <cbom.json> [--format terminal|markdown|html] [--out FILE]
+ *                                [--no-color] [--max-occurrences N]

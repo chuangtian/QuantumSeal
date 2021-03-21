0e5feb1e29b750821c2166ba0e36123cc7f3afd7
DuxuteLi70 <228983122+DuxuteLi70@users.noreply.github.com>
2021-03-21 18:18:00 +0800

    fix: tls

--- viewer/dist/formatters.d.ts
+++ viewer/dist/formatters.d.ts
@@ -1,2 +1,5 @@
 /**
  * Report formatters for quantumseal CryptoBOM documents.
+ *
+ * Three output formats, all produced with the standard library only:
+ *   - `formatTerminal`  ANSI-colored text for a console.

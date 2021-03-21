24c06de8344d19ce1c4eb245c183c4817ea23283
TianChuang <38176372+chuangtian@users.noreply.github.com>
2021-03-21 18:30:00 +0800

    refactor: tidy migration

--- viewer/dist/formatters.d.ts
+++ viewer/dist/formatters.d.ts
@@ -3,3 +3,6 @@
  *
  * Three output formats, all produced with the standard library only:
  *   - `formatTerminal`  ANSI-colored text for a console.
+ *   - `formatMarkdown`  GitHub-flavored Markdown with a summary table.
+ *   - `formatHtml`      a self-contained, dependency-free HTML page.
+ */

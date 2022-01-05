283476f85f8bab85993f9a71981d041fd491f585
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-01-05 14:12:00 +0800

    chore: cryptography

--- viewer/dist/formatters.js
+++ viewer/dist/formatters.js
@@ -24,3 +24,18 @@ function palette(enabled) {
             bold: "",
             dim: "",
             red: "",
+            yellow: "",
+            blue: "",
+            green: "",
+            magenta: "",
+        };
+    }
+    return {
+        reset: "\u001b[0m",
+        bold: "\u001b[1m",
+        dim: "\u001b[2m",
+        red: "\u001b[31m",
+        yellow: "\u001b[33m",
+        blue: "\u001b[34m",
+        green: "\u001b[32m",
+        magenta: "\u001b[35m",

81e3f3ea4513e397e63641b17cdcdc92e31a2c0b
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-02-18 17:55:00 +0800

    test: cover rust

--- viewer/dist/index.d.ts
+++ viewer/dist/index.d.ts
@@ -2,3 +2,4 @@
  * @quantumseal/viewer — public API.
  *
  * A dependency-free library for loading and rendering quantumseal CryptoBOM
+ * documents. See {@link parseCryptoBom} and the `format*` functions.

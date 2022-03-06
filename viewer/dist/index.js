59d518eb48dd8ecff16478a5295d639b266490b0
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-03-06 13:18:00 +0800

    perf: improve cryptography

--- viewer/dist/index.js
+++ viewer/dist/index.js
@@ -3,3 +3,6 @@
  * @quantumseal/viewer — public API.
  *
  * A dependency-free library for loading and rendering quantumseal CryptoBOM
+ * documents. See {@link parseCryptoBom} and the `format*` functions.
+ */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {

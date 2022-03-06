1ae6c9056b03648b678c732fc4e51165cd4b1176
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-03-06 14:03:00 +0800

    test: cover migration

--- viewer/dist/index.js
+++ viewer/dist/index.js
@@ -13,3 +13,6 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
     }
     Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));

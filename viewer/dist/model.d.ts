bb75892420ca1e4ce1502002e7e0f2b95da4d090
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-03-07 14:20:00 +0800

    test: cover ecc

--- viewer/dist/model.d.ts
+++ viewer/dist/model.d.ts
@@ -4,3 +4,10 @@
  * The viewer is dependency-free: it relies only on the Node.js standard library
  * and the JSON built into the language. This module turns an untyped parsed
  * JSON value into a strongly-typed {@link CryptoBom}, rejecting malformed input
+ * with clear errors.
+ *
+ * Reminder: a CryptoBOM is the output of *static analysis* of cryptographic
+ * indicators. It is not a security verdict or a cryptographic implementation.
+ */
+/** Quantum-risk classification codes emitted by the Rust CLI. */
+export type QuantumRisk = "low_resistant" | "moderate_grover" | "high_shor" | "critical_deprecated";

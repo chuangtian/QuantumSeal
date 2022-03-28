96e9da922265dcf00314b7d64599c451b139b2ee
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-03-28 09:45:00 +0800

    fix: ecc

---
+++ viewer/dist/model.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * Type definitions and a validating loader for quantumseal CryptoBOM documents.
+ *
+ * The viewer is dependency-free: it relies only on the Node.js standard library
+ * and the JSON built into the language. This module turns an untyped parsed
+ * JSON value into a strongly-typed {@link CryptoBom}, rejecting malformed input
+ * with clear errors.
+ *
+ * Reminder: a CryptoBOM is the output of *static analysis* of cryptographic
+ * indicators. It is not a security verdict or a cryptographic implementation.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.InvalidCryptoBomError = exports.BAND_ORDER = void 0;

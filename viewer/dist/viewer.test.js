6cd9eef2e85f3d935833ccd7de5793cbe359fa98
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-04-26 15:39:00 +0800

    style: clean up typescript

---
+++ viewer/dist/viewer.test.js
@@ -0,0 +1,12 @@
+"use strict";
+/**
+ * Tests for @quantumseal/viewer using the Node.js built-in test runner
+ * (`node --test`). No external test framework — dependency-free.
+ */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const strict_1 = __importDefault(require("node:assert/strict"));
+const node_test_1 = require("node:test");
+const formatters_1 = require("./formatters");

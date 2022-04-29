125e1f4da41e1b4d49777c4c6def94a2174a2e11
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-04-29 14:21:00 +0800

    feat: add ecc

--- viewer/dist/viewer.test.js
+++ viewer/dist/viewer.test.js
@@ -21,3 +21,14 @@ const SAMPLE = {
     summary: {
         files_scanned: 2,
         entries_visited: 4,
+        component_count: 2,
+        total_occurrences: 5,
+        priority_bands: { critical: 1, high: 1 },
+    },
+    components: [
+        {
+            id: "rsa",
+            name: "RSA",
+            category: "public_key",
+            quantum_risk: "high_shor",
+            quantum_risk_label: "High (Shor-breakable)",

79b15714505166c5a935e91cc92868ce3fbed0f1
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-07-12 16:27:00 +0800

    build: cryptography

---
+++ viewer/src/cli.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+/**
+ * quantumseal-view — CLI for rendering a CryptoBOM.
+ *
+ * Usage:
+ *   quantumseal-view <cbom.json> [--format terminal|markdown|html] [--out FILE]
+ *                                [--no-color] [--max-occurrences N]
+ *   quantumseal-view --help
+ *
+ * Reads a quantumseal CryptoBOM (JSON) and prints a formatted report. When no
+ * file is given, reads JSON from stdin. Dependency-free: Node standard library
+ * only.
+ */
+
+import { readFileSync, writeFileSync } from "node:fs";
+
+import {

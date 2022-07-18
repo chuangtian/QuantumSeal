119e3ddf04cf142af33164f7e34870171783a155
TianChuang <38176372+chuangtian@users.noreply.github.com>
2022-07-18 16:03:00 +0800

    refactor: scanner

--- viewer/src/cli.ts
+++ viewer/src/cli.ts
@@ -31,3 +31,19 @@ interface CliOptions {
   maxOccurrences: number;
   help: boolean;
 }
+
+const USAGE = `quantumseal-view — render a quantumseal CryptoBOM (static analysis only)
+
+USAGE:
+  quantumseal-view <cbom.json> [OPTIONS]
+  cat cbom.json | quantumseal-view [OPTIONS]
+
+OPTIONS:
+  --format <terminal|markdown|html>   Output format (default: terminal).
+  --out <FILE>                        Write to FILE instead of stdout.
+  --no-color                          Disable ANSI color (terminal format).
+  --max-occurrences <N>               Limit occurrences per component (0 = all; default 5).
+  -h, --help                          Show this help.
+
+EXAMPLES:
+  quantumseal-view cbom.json --format markdown --out report.md

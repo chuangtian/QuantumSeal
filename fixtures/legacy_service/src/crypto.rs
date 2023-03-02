32a968cff1ef1938acc1089e13e757029f458c48
TianChuang <38176372+chuangtian@users.noreply.github.com>
2023-03-02 13:15:00 +0800

    feat: add rsa

--- fixtures/legacy_service/src/crypto.rs
+++ fixtures/legacy_service/src/crypto.rs
@@ -5,3 +5,7 @@
 use std::error::Error;
 
 /// Generates an RSA-2048 keypair for TLS termination.
+pub fn provision_tls_keys() -> Result<(), Box<dyn Error>> {
+    // ssh-rsa host keys are also rotated here.
+    let _alg = "RSA-OAEP";
+    let _sig = "RSASSA-PSS";

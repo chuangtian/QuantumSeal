42fc260ef2d714f07b172e8af7dacba4394babbf
TianChuang <38176372+chuangtian@users.noreply.github.com>
2023-03-02 13:12:00 +0800

    fix: ecc

--- fixtures/legacy_service/src/crypto.rs
+++ fixtures/legacy_service/src/crypto.rs
@@ -1,3 +1,7 @@
 // Fixture: a service that (textually) uses legacy, quantum-vulnerable crypto.
 // This file exists so quantumseal has realistic indicators to inventory.
 // It is illustrative text — NOT a working or secure implementation.
+
+use std::error::Error;
+
+/// Generates an RSA-2048 keypair for TLS termination.

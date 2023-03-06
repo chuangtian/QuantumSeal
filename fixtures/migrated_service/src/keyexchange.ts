d1280630b9b622a3802a450e1ed95b843b97937a
TianChuang <38176372+chuangtian@users.noreply.github.com>
2023-03-06 16:12:00 +0800

    feat: add cbom

--- fixtures/migrated_service/src/keyexchange.ts
+++ fixtures/migrated_service/src/keyexchange.ts
@@ -1,2 +1,5 @@
 // Fixture: a service mid-migration to post-quantum cryptography.
 // Illustrative text only — not a working implementation.
+
+/**
+ * Hybrid key establishment during the transition period.

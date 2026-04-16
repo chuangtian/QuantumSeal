8350c759b93e39aa886fc076cfd9cf5370f1c006
TianChuang <38176372+chuangtian@users.noreply.github.com>
2026-04-17 03:24:00 +0800

    feat: cli

--- viewer/dist/index.d.ts
+++ viewer/dist/index.d.ts
@@ -4,3 +4,5 @@
  * A dependency-free library for loading and rendering quantumseal CryptoBOM
  * documents. See {@link parseCryptoBom} and the `format*` functions.
  */
+export * from "./model";
+export * from "./formatters";
